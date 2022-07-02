1a18df305a748a738a312a706a5c8b90340e9d83
pallini <shinpall1ni@gmail.com>
2022-07-02 13:22:33 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# bot-template
+
+Discord Botを作成するためのコードをまとめたものです。
+
+使用している言語はGoです。
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+var s *discordgo.Session
+
+var (
+	GuildID  string
+	BotToken string
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	GuildID = os.Getenv("GUILDID")
+	BotToken = os.Getenv("BOTTOKEN")
+}
+
+func init() {
+	var err error
+	s, err = discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	log.Println("Gracefully shutting down.")
+}
